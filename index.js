c9cd8e2685540eba9f9bb780040c023e3f759c33
Immanuel Kinuthia <immanuel.kinuthia@student.moringaschool.com>
2025-06-20 11:57:00 +0300

    Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = (drivers) => {
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = (drivers) => {
+  return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (multiplier) => {
+  return function(fare) {
+    return fare * multiplier;
+  }
+};
+
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(drivers, selectionFunction) {
+  return selectionFunction(drivers);
+};
+
